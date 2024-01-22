a9c1403f30cb951521a7043e428e2d4881813644
rayvanahnaf <rayvanahnaf00@gmail.com>
2024-01-22 13:54:51 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        13: '3.25rem',
+      }
+    },
+  },
+  plugins: [],
+}
